6d149873a19a4f204d824ee8948145d26a9471b2
JedLi-2014 <67016927+JedLi-2014@users.noreply.github.com>
2021-11-03 09:38:11 -0700

    Add files via upload

--- jed/tryingjs.js
+++ jed/tryingjs.js
@@ -5,3 +5,10 @@ function add(){
     c = a + b;
     document.getElementById("answer").value= c;
 }
+function subtract(){
+    var a1,b1,c1;
+    a1 = Number(document.getElementById("first1").value);
+    b1 = Number(document.getElementById("second1").value);
+    c1 = a1 - b1;
+    document.getElementById("answer1").value= c1;
+}
